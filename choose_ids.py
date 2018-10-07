9a5619354ee1c938f54d9213bff1de79ae5d8ac3
Ghulam Murtaza <gmurtaza404@gmail.com>
2018-10-07 15:09:09 +0500

    first commit

---
+++ choose_ids.py
@@ -0,0 +1,42 @@
+"""
+    import choose_id.py to use the algorithm
+    choose_id() function takes, a list of item, where each item should have a "memory_footprint" and a "value" attribute. 
+    It can have additional attributes, and the resulting list will retain those values. and the second parameter is the maxweight.
+    Maximum memory the webpage is allowed to consume.
+    Make sure, that memory footprints and maxweight have same unit.
+"""
+def choose_ids(items, maxweight):
+    # knapsack solution
+    result = [] # knapsack
+    
+    def bestvalue(i, j):
+        if i == 0: return 0
+        value= items[i - 1]["value"]
+        weight= items[i - 1]["memory_footprint"]
+        
+        if weight > j:
+            return bestvalue(i - 1, j)
+        else:
+            return max(bestvalue(i - 1, j),
+                       bestvalue(i - 1, j - weight) + value)
+
+    j = maxweight
+    result = []
+    
+    for i in xrange(len(items), 0, -1):
+        if bestvalue(i, j) != bestvalue(i - 1, j):
+            result.append(items[i - 1])
+            j -= items[i - 1]["memory_footprint"]
+    result.reverse()
+    
+    return bestvalue(len(items), maxweight), result
+
+#driver function
+def main():
+    knapsack_list = [{"id":1,"memory_footprint":12,"value":2},{"id":2,"memory_footprint":21,"value":3},{"id":3,"memory_footprint":11,"value":1}]
+    memory_capacity = 35
+    
+    print choose_ids(knapsack_list, memory_capacity)    
+
+
+#main()
---
+++ generate_transcoded_page.py
@@ -0,0 +1,40 @@
+"""
+    This script assumes that there is a json file available for a given page. Lets say a user wants to fetch, www.google.com page
+    there exists a folder with name www.google.com and that folder has the base page with all the elements it need. Also, it has
+    a JSON file that contains the information regarding, which tag consumes how much memory.
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def generate_transcoded_page():
+    print "generating page"
+
+
+
+generate_transcoded_page()
